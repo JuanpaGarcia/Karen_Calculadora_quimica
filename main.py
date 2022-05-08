fb956eb50f8c5a0dfd764db7c1f4c800732210c1
JuanpaGarcia <garciaperezjuanpablo7@gmail.com>
2022-05-08 11:59:46 -0500

    Terminada funcionalidad de Conversion de temperatura

--- main.py
+++ main.py
@@ -20,9 +20,56 @@ def print_menu_opciones():
     print("\t\t5)\tTerminar programa\n")
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def menu_temperatura():
+    print("\tMenu de opciones de conversion de temperatura:")
+    print("\t\t1)\tConvertir de C -> F")
+    print("\t\t2)\tConvertir de C -> K")
+    print("\t\t3)\tConvertir de F -> C")
+    print("\t\t4)\tConvertir de F -> K")
+    print("\t\t5)\tConvertir de K -> C")
+    print("\t\t6)\tConvertir de K -> F")
+
+
+def conversion_celsius(a, b):
+    #a selecionador si F o K
+    if a == 1: #Covnvertir celsius a F
+        return (9/5) * b + 32
+    else: #Covnvertir celsius a K
+        return b + 273.15
+
+
+def conversion_farenheit(a, b):
+    #a selecionador si F o K
+    if a == 1: #Covnvertir F a C
+        return (b - 32)/1.8
+    else: #Covnvertir F a K
+        return (b + 459.67)/1.8
+
+def conversion_kelvin(a, b):
+    #a selecionador si F o K
+    if a == 1: #Covnvertir K a C
+        return b - 273.15
+    else: #Covnvertir k a F
+        return b * 1.8 -459.67
+
+
+def calcular_conversion_temp():
+    menu_temperatura()
+    opcion_menu_t = int(input("Introduce la opcicion a convertir: \n"))
+    temperatura_a_convertir = float(input("Introduce la temperatura a convertir (solo unidades): \n"))
+    if opcion_menu_t == 1:
+        print(temperatura_a_convertir,"grados celsius es equivalente a",conversion_celsius(1,temperatura_a_convertir), "grados Farenheit")
+    elif opcion_menu_t == 2:
+        print(temperatura_a_convertir,"grados celsius es equivalente a",conversion_celsius(2,temperatura_a_convertir), "grados Kelvin")
+    elif opcion_menu_t == 3:
+        print(temperatura_a_convertir,"grados Farenheit es equivalente a",conversion_farenheit(1,temperatura_a_convertir), "grados Celsius")
+    elif opcion_menu_t == 4:
+        print(temperatura_a_convertir,"grados Farenheit es equivalente a",conversion_farenheit(2,temperatura_a_convertir), "grados Kelvin")
+    elif opcion_menu_t == 5:
+        print(temperatura_a_convertir,"grados Kelvin es equivalente a",conversion_kelvin(1,temperatura_a_convertir), "grados Celsius")
+    elif opcion_menu_t == 6:
+        print(temperatura_a_convertir,"grados Kelvin es equivalente a",conversion_kelvin(2,temperatura_a_convertir), "grados Farenheit")
+
 
 #Cargar variables de menu
 
@@ -30,11 +77,12 @@ m_molar: int = 1
 ley_gas: int = 2
 tabla_p: int = 3
 conversion: int = 4
+terminar: int = 5
 
 print_menu_opciones()
 opcion = int(input("Introduce la opcicion a escoger: \n"))
 
-while opcion <= conversion:
+while True:
     if opcion == m_molar:
         print("Opcion masa molar")
 
@@ -46,12 +94,15 @@ while opcion <= conversion:
 
     elif opcion == conversion:
         print("Opcion para conversion de temperatura")
+        calcular_conversion_temp()
+    elif opcion == terminar:
+        break
     else:
         print("Seleccionar opcion valida")
     print_menu_opciones()
     opcion = int(input("Introduce la opcicion a escoger: \n"))
 
-print("Fin de calculadora ")
+print("Fin de calculadora, Gracias ")
 
 
 
