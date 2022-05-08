44f3eaef77e53ae931977207fa6ca4ca0965d981
JuanpaGarcia <garciaperezjuanpablo7@gmail.com>
2022-05-08 11:14:12 -0500

    Integración de menu y de funcion para presentar menu al usuario

--- main.py
+++ main.py
@@ -9,8 +9,50 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
+def print_menu_opciones():
+    print("Proyecto de programacion y algotitmos\n")
+    print("Calculadora química\n\n")
+    print("\tMenu de opciones:")
+    print("\t\t1)\tCalculadora de masa molar\n")
+    print("\t\t2)\tCalculadora de ley de gases\n")
+    print("\t\t3)\tConsulta de elemento de tabla periodica\n")
+    print("\t\t4)\tCalculadora de conversiones\n")
+    print("\t\t5)\tTerminar programa\n")
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+#Cargar variables de menu
+
+m_molar: int = 1
+ley_gas: int = 2
+tabla_p: int = 3
+conversion: int = 4
+
+print_menu_opciones()
+opcion = int(input("Introduce la opcicion a escoger: \n"))
+
+while opcion <= conversion:
+    if opcion == m_molar:
+        print("Opcion masa molar")
+
+    elif opcion == ley_gas:
+        print("Opcion ley de gases")
+
+    elif opcion == tabla_p:
+        print("Opcion consultar tabla periodica")
+
+    elif opcion == conversion:
+        print("Opcion para conversion de temperatura")
+    else:
+        print("Seleccionar opcion valida")
+    print_menu_opciones()
+    opcion = int(input("Introduce la opcicion a escoger: \n"))
+
+print("Fin de calculadora ")
+
+
+
+
